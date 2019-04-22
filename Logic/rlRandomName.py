5b4c8a636c58e0a9bc32572bc4a5055a1482b02a
AlexLemna <alexanderlemna@gmail.com>
2019-04-22 14:01:16 -0500

    rlRandomName.py now functional both generally and for debug
    Famous last words, right?
    
    Anyway, updated the target file location. Added a "if __name__ == '__main__'" statement for debugging behavior.  Copied over the CWD_home() function I used in rosa.py.
    
    rlRandomName.py still only contains one function, but it's a start.

--- Logic/rlRandomName.py
+++ Logic/rlRandomName.py
@@ -1,28 +1,36 @@
 # ----------------------------------------
-# Gets random text from /Data/simple_data/sample.txt
+# rlRandomName.py
 # ----------------------------------------
+# A logic module for Alex's "Project Rosa" that contains functions for randomly generating names.
 
 import os
-import pathlib
 import random
 import sys
 
 # Defining empty list. Explicit variable declaration isn't necessary in Python, but I like it.
 contents = []
 
-# target files is at "F:\OneDrive\CODE\Projects\npc-name-generator\Data\simple data\sample.txt"
-def fRandomTextSampleData ():
-  os.chdir (os.path.dirname (sys.argv[0])) # <-- Sets working directory to home folder. From https://stackoverflow.com/questions/1432924/python-change-the-scripts-working-directory-to-the-scripts-own-directory
-  os.chdir ( ".." )
-  os.chdir ( ".\Data\SimpleData" )
-  with open('sample.txt', 'r') as fileData:
-    contents = fileData.readlines()
-    return ( random.choice(contents) )
-  
-# fRandomTextSampleData()
+def CWD_home(): # Ensures that the current working directory is set to the home directory of the active script. From https://stackoverflow.com/questions/1432924/python-change-the-scripts-working-directory-to-the-scripts-own-directory
+    os.chdir (os.path.dirname (sys.argv[0]))
 
-# ------------------------------------
-# RANDOM SNIPPETS FOR DEBUGGING:
-#
-# with pathlib.Path.cwd() as cwd:
-#   print (f"My cwd is {cwd}.")
+# target files is at \Data\SampleData.txt"
+def SampleData ():
+  if __name__ == "__main__": # Checks if this program is running as the main script (only hallens for debugging purposes)
+    CWD_home()
+    os.chdir ("..")
+    os.chdir ( ".\Data" )
+    with open('SampleData.txt', 'r') as fileData:
+      contents = fileData.readlines()
+      print ( random.choice(contents) )
+  elif __name__ == "Logic.rlRandomName": # This is the normal module behavior - it's being run from somewhere else.
+    CWD_home()
+    os.chdir ( ".\Data" )
+    with open('SampleData.txt', 'r') as fileData:
+      contents = fileData.readlines()
+      return ( random.choice(contents) )
+  else: # I'm honestly not sure what situation wouldn't fit into the two statements above. If that happens, though, I want to pause the program and take a look!
+    import pdb
+    pdb.set_trace() # Creates a breakpoint.
+
+SampleData()
+# print ( f"{SampleData}" )
