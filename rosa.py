203a90844f6744ded24ad850f5ea8377786f3565
AlexLemna <alexanderlemna@gmail.com>
2019-04-21 21:13:47 -0500

    Update rosa.py
    Added a main menu interface

--- rosa.py
+++ rosa.py
@@ -26,5 +26,44 @@ print ("Getting some local modules... ", end="")
 print ("done.")
 
 # SOME FUNCTIONS AND VARIABLES FOR EASY REFERENCE
+def MainMenu():
+    print ()
+    print (10 * "-", "Rosa.py Main Menu", 10 * "-" )
+    print ("What sorts of names would you like to generate?")
+    print ("     1. First names")
+    print ("     2. First names, female-only")
+    print ("     3. First names, male-only")
+    print ("     4. Last names")
+    print ("     5. Full names")
+    print ("     6. Full names, female-only")
+    print ("     7. Full names, male-only")
+    print ("X or 0. Exit program")
+    print ()
+    menuChoice = input ("Enter your choice: ")
+    print ()
+
+    if menuChoice == "1":
+        pass
+    elif menuChoice == "2":
+        pass
+    elif menuChoice == "3":
+        pass
+    elif menuChoice == "4":
+        pass
+    elif menuChoice == "5":
+        pass
+    elif menuChoice == "6":
+        pass
+    elif menuChoice == "7":
+        pass
+    elif menuChoice == '0' or menuChoice == 'X' or menuChoice == "x":
+        see_rosa_run is False
+    else:
+        print ("Look, I'm just a computer program. I've got all the time to waste in the world, and I don't get bored. You can keep entering invalid inputs, or you can actually follow instructions. Your call.")
 
 # *** MAIN PROGRAM STARTS HERE ***
+see_rosa_run = True
+if see_rosa_run is True:
+    MainMenu()
+else:
+    sys.exit(0)
