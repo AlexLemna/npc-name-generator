caa7d4835a181315029fc9e5470a6041b677b240
AlexLemna <alexanderlemna@gmail.com>
2019-04-22 11:31:46 -0500

    Add startup logic, textwrap to rosa.py
    Created 'RosaSetup' function, which brings in some logic and startup dialogs that had previously been stored in LogicController.py. This reflects a shift towards rosa.py being the main script, not LogicController.
    
    Also, the startup dialogs needed some textwrapping so I added that.

--- rosa.py
+++ rosa.py
@@ -15,18 +15,77 @@ print ("Getting some modules from the standard library... ", end="")
 import os
 import pathlib
 import sys
+import textwrap
 print ("done.")
 
 # MODULES FROM PYPI (the Python community)
 # none
 
 # MY MODULES
-print ("Getting some local modules... ", end="")
-# from Logic import LogicController
-print ("done.")
+def myModules():
+    print ("Getting some local modules... ", end="")
+    # from Logic import LogicController
+    print ("done.")
 
 # SOME FUNCTIONS AND VARIABLES FOR EASY REFERENCE
-def MainMenu():
+def CWD_home(): # Ensures that the current working directory is set to the home directory of the active script. From https://stackoverflow.com/questions/1432924/python-change-the-scripts-working-directory-to-the-scripts-own-directory
+    os.chdir (os.path.dirname (sys.argv[0]))
+
+def printw(x): # Textwrapping for regular 'print' commands.
+    print ( textwrap.fill (x, width = 70))
+
+def inputw(x): # Textwrapping for user input prompts.
+    input ( textwrap.fill (x, width=70))
+
+def RosaSetup(): # Contains setup instructions.
+    print ("Looking for UI, Logic, and Data directories...")
+    missingDirectories = []
+    CWD_home ()
+    with pathlib.Path.cwd() as cwd:
+        uiDirectory = cwd / 'UI'
+        logicDirectory = cwd / 'Logic'
+        dataDirectory = cwd / 'Data'
+    
+    if os.path.exists (uiDirectory):
+        print ("   ...UI directory located.")
+    else: 
+        print ("   ...UI directory not found.")
+        missingDirectories.append("UI")
+    if os.path.exists (logicDirectory):
+        print ("   ...logic directory located.")
+    else: 
+        print ("   ...logic directory not found.")
+        missingDirectories.append("Logic")
+    if os.path.exists (dataDirectory):
+        print ("   ...data directory located.")
+    else: 
+        print ("   ...data directory not found.")
+        missingDirectories.append("Data")
+
+    if os.path.exists (uiDirectory) == False or os.path.exists (logicDirectory) == False or os.path.exists (dataDirectory) == False:
+        print (f"I can't find following directories: {missingDirectories}.")
+        print ()
+        if os.path.exists (uiDirectory) == False:
+            printw ("The UI directory is missing, but this program can run perfectly fine from the command-line interface (CLI). User input specifying alternate locations for the UI directory is not supported at this time, but will be added in future versions soon(TM). For now, though, you're stuck with the CLI.")
+            print()
+            menuChoice = inputw ("If you would like to exit the program now, enter X or 0. Otherwise, press any key to continue. ")
+            if menuChoice == '0' or menuChoice == 'X' or menuChoice == "x":
+                sys.exit(0) # Exits the program. "0" is the exit code for a successful program exiting. "1" is the exit code for a program exiting due to an error.
+            else:
+                pass
+        else:
+            printw ("This is a problem - this program will not run properly without a logic or data directory. User input specifying alternate locations for these directories is not supported at this time, but will be added in future versions soon(TM).")
+            input ("Press any key to quit.")
+            sys.exit(1) # Exits the program. "0" is the exit code for a successful program exiting. "1" is the exit code for a program exiting due to an error.
+    else:
+        print ("All directories present. Proceeding to get local modules.")
+    myModules()
+    print("Setup complete.")
+    print()
+
+
+
+def MainMenu(): # Contains logic and display instructions for the main menu.
     print ()
     print (10 * "-", "Rosa.py Main Menu", 10 * "-" )
     print ("What sorts of names would you like to generate?")
@@ -62,6 +121,8 @@ def MainMenu():
         print ("Look, I'm just a computer program. I've got all the time to waste in the world, and I don't get bored. You can keep entering invalid inputs, or you can actually follow instructions. Your call.")
 
 # *** MAIN PROGRAM STARTS HERE ***
+RosaSetup()
+
 see_rosa_run = True
 if see_rosa_run is True:
     MainMenu()
