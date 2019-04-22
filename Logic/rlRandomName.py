1fb506d3ec026fdcec8e543ddf488488bcb46dfc
AlexLemna <alexanderlemna@gmail.com>
2019-04-22 12:49:30 -0500

    renamed RandomTextSampleData to rlRandomName

--- Logic/rlRandomName.py
+++ Logic/rlRandomName.py
@@ -13,13 +13,13 @@ contents = []
 # target files is at "F:\OneDrive\CODE\Projects\npc-name-generator\Data\simple data\sample.txt"
 def fRandomTextSampleData ():
   os.chdir (os.path.dirname (sys.argv[0])) # <-- Sets working directory to home folder. From https://stackoverflow.com/questions/1432924/python-change-the-scripts-working-directory-to-the-scripts-own-directory
-  os.chdir ("..")
+  os.chdir ( ".." )
   os.chdir ( ".\Data\SimpleData" )
   with open('sample.txt', 'r') as fileData:
     contents = fileData.readlines()
     return ( random.choice(contents) )
   
-fRandomTextSampleData()
+# fRandomTextSampleData()
 
 # ------------------------------------
 # RANDOM SNIPPETS FOR DEBUGGING:
