e048e7cb2053b43a1a9a39514d8530532960c6e6
Alex Lemna <alexanderlemna@gmail.com>
2019-04-24 10:16:52 -0500

    Updated rlRandomName with better debugging and error message
    Removed pdb.set_trace, as the Python interpreter in VS Code threw up a warning for possible incompatibility with its own debugger. Replaced it with a very descriptive error message.
    
    Also fixed rlRandomName's behavior when LogicController uses it as a module. Not useful except for debugging, but still good to have.

--- Logic/rlRandomName.py
+++ Logic/rlRandomName.py
@@ -14,22 +14,55 @@ def CWD_home(): # Ensures that the current working directory is set to the home 
 
 # target files is at \Data\SampleData.txt"
 def SampleData ():
-  if __name__ == "__main__": # Checks if this program is running as the main script (only hallens for debugging purposes)
+  if __name__ == "Logic.rlRandomName": # This is the normal module behavior - it's being run from somewhere else.
+    CWD_home()
+    os.chdir ( ".\Data" )
+    with open('SampleData.txt', 'r') as fileData:
+      contents = fileData.readlines()
+      return ( random.choice(contents) )
+
+  elif __name__ == "__main__": # Checks if this program is running as the main script (only happens for debugging purposes)
     CWD_home()
     os.chdir ("..")
     os.chdir ( ".\Data" )
     with open('SampleData.txt', 'r') as fileData:
       contents = fileData.readlines()
       print ( random.choice(contents) )
-  elif __name__ == "Logic.rlRandomName": # This is the normal module behavior - it's being run from somewhere else.
+  
+  elif __name__ == "rlRandomName": # Checks if this program is running as a module from another script inside its home directory (only happens for debugging purposes)
     CWD_home()
+    os.chdir ("..")
     os.chdir ( ".\Data" )
     with open('SampleData.txt', 'r') as fileData:
       contents = fileData.readlines()
       return ( random.choice(contents) )
-  else: # I'm honestly not sure what situation wouldn't fit into the two statements above. If that happens, though, I want to pause the program and take a look!
-    import pdb
-    pdb.set_trace() # Creates a breakpoint.
+
+  else: # I'm honestly not sure what situation wouldn't fit into the statements above.
+    from inspect import currentframe, getframeinfo
+    frameinfo = getframeinfo ( currentframe())
+
+    print ("")
+    print ("A wild UNEXPECTED ERROR appeared!")
+    print ( f"REF: The file location is {frameinfo.filename}." )
+    print ( f"REF: This error message is around line {frameinfo.lineno} of that file.")
+    print ( "   SUMMARY:")
+    print ( "   This section of code has some 'if' and 'else if' statements that should")
+    print ( "   cover every situation. This error is the 'else' statement, and it means")
+    print ( "   a situation occured that I didn't forsee.")
+    print ( "   SPECIFICS:")
+    print ( "   The error occured in a file called 'rlRandomName.py'. The file behaves")
+    print ( "   differently depending on if it is running by itself or as a 'module' being")
+    print ( "   called by a different file. It can tell how it is being run by checking")
+    print ( "   a variable called __name__ against some predetermined values. It expects")
+    print ( "   __name__'s value to be 'rlRandomName', 'Logic.rlRandomName', or '__main__'.")
+    print ( f"   Instead, the value is '{__name__}'.")
+    print ( "   WHAT YOU SHOULD DO:")
+    print ( "   Take a screenshot and contact Alex. Also, tell him to create some kind of")
+    print ( "   error logging system so you don't have to manually ask him for help every")
+    print ( "   time he messes up.")
+    
+    # import pdb
+    # pdb.set_trace() # Creates a breakpoint.
 
 SampleData()
 # print ( f"{SampleData}" )
