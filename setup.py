05b9cafadbb32b014dfb046ce97da814abd332dd
Alex Lemna <25907117+AlexLemna@users.noreply.github.com>
2019-05-09 15:41:56 -0500

    FEAT (untested): Add functional setup.py
    Based off of: https://github.com/kennethreitz/setup.py
    
    Associated with issue #22

--- setup.py
+++ setup.py
@@ -1,6 +1,117 @@
-from setuptools import setup
+import io
+import os
+import sys
+from shutil import rmtree
 
-setup (
-    name = "rosevomit",
-    python_requires = '>=3.7.2', # I don't actually know what versions I'm compatible with. This is just a paceholder while I figure this out, and to see if it shows up under GitHub dependencies.
+from setuptools import find_packages, setup, Command
+
+# Package meta-data
+NAME = "Rosevomit"
+DESCRIPTION = "A random name generator. Also a beginner's exercise in Python."
+URL = "https://github.com/AlexLemna/rosevomit"
+EMAIL = "25907117+AlexLemna@users.noreply.github.com"
+REQUIRES_PYTHON = ">=3.7.3"
+AUTHOR = "Alex Lemna"
+VERSION = '0.10.1-m1'
+
+here = os.path.abspath (os.path.dirname (__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open (os.path.join (here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open (os.path.join (here, project_slug, '__version__.py')) as f:
+        exec (f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status ('Removing previous builds…')
+            rmtree (os.path.join (here, 'dist'))
+        except OSError:
+            pass
+
+        self.status ('Building Source and Wheel (universal) distribution…')
+        os.system ('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status ('Uploading the package to PyPI via Twine…')
+        os.system ('twine upload dist/*')
+
+        self.status ('Pushing git tags…')
+        os.system ('git tag v{0}'.format(about['__version__']))
+        os.system ('git push --tags')
+        
+sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+
+    # I don't use these variables because I don't have anything required or recommended:
+    # install_requires=REQUIRED,
+    # extras_require=EXTRAS,
+
+    include_package_data=True,
+    license='GPL-3.0',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
