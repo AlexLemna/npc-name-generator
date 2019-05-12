1fec811abafa3c3c7e3083044a5c22c9f235f80a
Alex Lemna <25907117+AlexLemna@users.noreply.github.com>
2019-05-12 15:45:35 -0500

    FEAT (tests): Add tests for last, full name gen
    Related to issue #24

--- dev-tests/test_Rosevomit.py
+++ dev-tests/test_Rosevomit.py
@@ -63,6 +63,78 @@ class RosevomitTestCase(unittest.TestCase):
             pass
         self.assertEqual (number_names_to_generate, len (names_generated))
 
+    def test_last_name_generation(self):
+        """Do we get the number of names we expect?"""
+        number_names_to_generate = random.randint (2, 200)
+        old_stdout = sys.stdout
+
+        test_stdout = StringIO()  # io.StringIO is in-memory stream for text I/O.
+        sys.stdout = test_stdout  # Redirecting stdout to stream.
+        LogicController.gen ("last", number_names_to_generate)  # Actually running test
+
+        sys.stdout = old_stdout  # Resetting stdout to print to terminal again
+        result = test_stdout.getvalue()  # Reading the test results from memory
+        names_generated = result.split('\n')
+        if names_generated[-1] == "":   # Sometimes the results end with a blank line.
+            del names_generated[-1]     # We get rid of that.
+        else:
+            pass
+        self.assertEqual (number_names_to_generate, len (names_generated))
+
+    def test_full_name_generation(self):
+        """Do we get the number of names we expect?"""
+        number_names_to_generate = random.randint (2, 200)
+        old_stdout = sys.stdout
+
+        test_stdout = StringIO()  # io.StringIO is in-memory stream for text I/O.
+        sys.stdout = test_stdout  # Redirecting stdout to stream.
+        LogicController.gen ("full", number_names_to_generate)  # Actually running test
+
+        sys.stdout = old_stdout  # Resetting stdout to print to terminal again
+        result = test_stdout.getvalue()  # Reading the test results from memory
+        names_generated = result.split('\n')
+        if names_generated[-1] == "":   # Sometimes the results end with a blank line.
+            del names_generated[-1]     # We get rid of that.
+        else:
+            pass
+        self.assertEqual (number_names_to_generate, len (names_generated))
+
+    def test_female_full_name_generation(self):
+        """Do we get the number of names we expect?"""
+        number_names_to_generate = random.randint (2, 200)
+        old_stdout = sys.stdout
+
+        test_stdout = StringIO()  # io.StringIO is in-memory stream for text I/O.
+        sys.stdout = test_stdout  # Redirecting stdout to stream.
+        LogicController.gen ("fullfemale", number_names_to_generate)  # Actually running test
+
+        sys.stdout = old_stdout  # Resetting stdout to print to terminal again
+        result = test_stdout.getvalue()  # Reading the test results from memory
+        names_generated = result.split('\n')
+        if names_generated[-1] == "":   # Sometimes the results end with a blank line.
+            del names_generated[-1]     # We get rid of that.
+        else:
+            pass
+        self.assertEqual (number_names_to_generate, len (names_generated))
+
+    def test_male_full_name_generation(self):
+        """Do we get the number of names we expect?"""
+        number_names_to_generate = random.randint (2, 200)
+        old_stdout = sys.stdout
+
+        test_stdout = StringIO()  # io.StringIO is in-memory stream for text I/O.
+        sys.stdout = test_stdout  # Redirecting stdout to stream.
+        LogicController.gen ("fullmale", number_names_to_generate)  # Actually running test
+
+        sys.stdout = old_stdout  # Resetting stdout to print to terminal again
+        result = test_stdout.getvalue()  # Reading the test results from memory
+        names_generated = result.split('\n')
+        if names_generated[-1] == "":   # Sometimes the results end with a blank line.
+            del names_generated[-1]     # We get rid of that.
+        else:
+            pass
+        self.assertEqual (number_names_to_generate, len (names_generated))
+
 # Now, need to add similar tests for the rest of 'LogicController.gen'
 
 unittest.main()
