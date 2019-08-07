3d1179b8ee063b2f1958c282a9741603718b631d
Alex Lemna <25907117+AlexLemna@users.noreply.github.com>
2019-08-07 14:53:57 -0500

    ADJ (setname function): Add ability to accept arguments
    Changed in 'rosevomit/programlogic/workwithprogramfiles.py'

--- rosevomit/programlogic/workwithprogramfiles.py
+++ rosevomit/programlogic/workwithprogramfiles.py
@@ -6,11 +6,11 @@
 import os
 
 
-def setname():
-    """Determines a valid filename (_filename) for the temp file to be generated. Checks to make sure that _filename is not already in use. If it is, it adds a number on the end of _filename and keeps checking to see if the new _filename is unused, incrementing the number until it finds an unused _filename. """
+def setname(ARG_basename):
+    """Determines a valid filename for based off a desired 'ARG_basename'. Checks to make sure that _filename is not already in use. If it is, it adds a number on the end of _filename and keeps checking to see if the new _filename is unused, incrementing the number until it finds an unused _filename. """
     _startnum = 1
-    _filename = f"log{_startnum}.txt"
+    _filename = f"{ARG_basename}{_startnum}.txt"
     while os.path.isfile (_filename) is True:
         _startnum = _startnum + 1
-        _filename = f"log{_startnum}.txt"
+        _filename = f"{ARG_basename}{_startnum}.txt"
     return _filename
