2ce29a544a6ecf22a39267ab671f367d7bbd3e57
Alex Lemna <25907117+AlexLemna@users.noreply.github.com>
2019-08-24 18:23:26 -0500

    Complete suncalc demo

--- rosevomit/suncalc.py
+++ rosevomit/suncalc.py
@@ -1,7 +1,6 @@
 from math import cos, sin, tan, acos, asin, atan2
 from math import degrees, radians  # for converting
 from math import fmod  # returns the remainder (the mathematical modulus) of a division expression
-import textwrap
 
 # Calculating the position of the sun in Earth's sky is weird and kinda mind-bending. The terminology and equations we use for tracking things moving across the sky have been developed over hundreds (thousands?) of years, and they've improved along with our understanding of the universe, but we still have to switch between a few different paradigms while we're doing this. Professional astronomers and experienced amateurs might be able to hold all these interrelated concepts in their head, but me? I compensate by using lots of notes.
 # There are three different coordinate systems we need to switch between. We use ecliptic coordinates (technically, geocentric ecliptic coordinates) to calculate the motion of the bodies in the Antikytheran solar system (or, technically, the apparent motion of the sun with respect to Antikythera). The 'ecliptic' that the coordinate system is based on refers to the plane of Antikythera's average orbit around its sun.
@@ -10,10 +9,25 @@ import textwrap
 # I make some simplifications in Antikythera's system. First, the real orbit *is* the average orbit. Deviations from the average orbit are fixed by the unseen "Silent Cosmic Bureaucracy's Conference on Weights and Measures' Department of Continuity Physics' Enforcement Bureau's Orbital Mechanics Division," often abbreviated to CoWMDoCPEB-OrbMech). Second, Antikythera's orbit is perfectly circular and is completed in exactly 365 days (with each day, or rotation of Antikythera arounds its axis, lasting exactly 24 hours.)
 
 
+def angle_sanity_fix (angle_value):
+    if 0 <= angle_value < 360:
+        pass
+    elif angle_value < 0:
+        angle_value = angle_value + 360
+    elif angle_value > 360:
+        angle_value = angle_value - 360
+    elif angle_value == 360:
+        angle_value = 0
+    else:
+        raise ValueError
+    return angle_value
+
+
 def defaultsiteprompt() -> bool:  # type: ignore, because otherwise mypy seems to want an explicit return statement in the 'else' section
     """Ask for latitude and longitude"""
-    userinput: str = input (textwrap.fill ("The default site for this calculation is Cordu. Enter [Y] to accept this site, or [N] to input custom coordinates. "))
-    if userinput == "Y" or userinput == "y":
+    print ("The default site for this calculation is Cordu.")
+    userinput: str = input ("Enter nothing or [Y] to accept this site, or [N] to input custom coordinates. ")
+    if userinput == "Y" or userinput == "y" or userinput == "":
         return True
     elif userinput == "N" or userinput == "n":
         return False
@@ -22,8 +36,19 @@ def defaultsiteprompt() -> bool:  # type: ignore, because otherwise mypy seems t
         defaultsiteprompt ()
 
 
-def antikythera_rotation_angle():
+def antikythera_rotation_angle (seconds_elapsed):
     """Returns the 'Antikythera Rotation Angle', equivalent to Earth's 'Earth Rotation Angle' and similar to Earth's 'Greenwich Sidereal Time', though Earth's GST includes an adjustment for the procession of Earth's March equinox that is irrelevant for Antikythera."""
+    # validating
+    if 0 <= seconds_elapsed <= 86400:
+        pass
+    else:
+        raise ValueError
+    # calculating current rotation angle
+    hours_elapsed = seconds_elapsed / 3600
+    rotation_angle = hours_elapsed * 15
+    # sanity-checking the degrees
+    rotation_angle = angle_sanity_fix (rotation_angle)
+    return rotation_angle
 
 
 def solar_hour_angle():
@@ -33,8 +58,6 @@ def solar_hour_angle():
     """
 
 # defaults
-LATITUDE: float
-LONGITUDE: float
 AXIAL_TILT: float = 27.5
 ecliptic_longitude: float
 ecliptic_latitude: float
@@ -47,6 +70,7 @@ azimuth: float
 
 day: float = 1
 dayfrom0: float = day - 1
+orbitaldaynumber = O_D_N = dayfrom0
 
 # Main program begins
 usedefaults: bool = defaultsiteprompt()
@@ -65,16 +89,18 @@ else:
 ϵ = AXIAL_TILT = OBLIQUITY_OF_THE_ECLIPTIC
 α = right_ascension = atan2 (cos(ϵ) * sin(λ), cos(λ))
 δ = declination = asin (sin(ϵ) * sin(λ))
-h = hour_angle
+h = hour_angle = antikythera_rotation_angle(O_D_N) + LATITUDE - degrees(α)
 
 # finally, we need to convert the ecliptic coordinates into horizontal coordinates
-A = azimuth
-a = altitude
+part1 = (-sin(LATITUDE) * cos(δ) * cos(h)) + (cos(LATITUDE) * sin(δ))
+part2 = cos(δ) * sin(h)
+A = azimuth = -atan2(part1, part2)
+a = altitude = asin ((sin(LATITUDE) * sin(δ)) + (cos(LATITUDE) * cos(δ) * cos(h)))
 
-with (-sin(LATITUDE) * cos(δ) * cos(h)) + (cos(LATITUDE) * sin(δ)) as part1, cos(δ) * sin(h) as part2:
-    A = -atan2(part1, part2)
 A = degrees(A)
-
-a = asin ((sin(LATITUDE) * sin(δ)) + (cos(LATITUDE) * cos(δ) * cos(h)))
 a = degrees(a)
 zenith: float = 90 - a
+
+print (f"The sun's azimuth is {A} degrees.")
+print (f"The sun's altitude is {a} degrees.")
+print (f"The sun's angle from the zenith is {zenith} degrees.")
